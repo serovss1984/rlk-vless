1f2ff49c4545852e7ace8dd71a2cd98e1c0f10d5
Alex Roll <sas@rollyk.ru>
2024-11-11 10:33:31 +0300

    Релиз биллинга

--- biling.js
+++ biling.js
@@ -6,7 +6,7 @@ const cron = require('node-cron');
 
 // Конфигурация для бота Telegram
 const TELEGRAM_BOT_API = process.env.TELEGRAM_BOT_API;
-const TELEGRAM_NOTIFICATION_TIME = process.env.TELEGRAM_NOTIFICATION_TIME || '09:00';
+const TELEGRAM_NOTIFICATION_TIME = process.env.TELEGRAM_NOTIFICATION_TIME || '12:00';
 
 // Функция отправки уведомлений в Telegram
 const sendTelegramNotification = (chatId, message) => {
@@ -27,8 +27,7 @@ const sendTelegramNotification = (chatId, message) => {
 // Функция для почасового списания абонентской платы
 const chargeUsersHourly = () => {
   const currentDate = moment().format('YYYY-MM-DD HH:mm:ss');
-
-  db.query('SELECT * FROM users WHERE balance > 0', (err, users) => {
+  db.query('SELECT * FROM users WHERE locked = 0', (err, users) => {
     if (err) {
       console.error('Ошибка получения пользователей:', err);
       return;
@@ -40,7 +39,6 @@ const chargeUsersHourly = () => {
           console.error('Ошибка получения тарифа:', err);
           return;
         }
-
         const hourlyRate = parseFloat(plan[0].price) / (30 * 24); // Почасовая стоимость тарифа
         const balance = parseFloat(user.balance);
 
@@ -60,8 +58,39 @@ const chargeUsersHourly = () => {
               console.error(`Ошибка блокировки ${user.chatId}:`, err);
               return;
             }
+            sendTelegramNotification(user.chatId, 'Ваш аккаунт заблокирован. Пополните счет для восстановления доступа.');
             console.log(`Блокировка ${user.chatId}`);
-            // sendTelegramNotification(user.chatId, 'Ваш баланс истек, аккаунт заблокирован. Пополните счет для восстановления доступа.');
+          });
+        }
+      });
+    });
+  });
+};
+
+// Функция разблокировки пользователей
+const UnlockUsers = () => {
+  const currentDate = moment().format('YYYY-MM-DD HH:mm:ss');
+  db.query('SELECT * FROM users WHERE locked = 1', (err, users) => {
+    if (err) {
+      console.error('Ошибка получения пользователей:', err);
+      return;
+    }
+    users.forEach(user => {
+      db.query('SELECT * FROM plans WHERE id = ?', [user.plan_id], (err, plan) => {
+        if (err) {
+          console.error('Ошибка получения тарифа:', err);
+          return;
+        }
+        const hourlyRate = parseFloat(plan[0].price) / (30 * 24); // Почасовая стоимость тарифа
+        const balance = parseFloat(user.balance);
+        if (balance >= hourlyRate) {
+          db.query('UPDATE users SET locked = 0 WHERE chatId = ?', [user.chatId], (err) => {
+            if (err) {
+              console.error(`Ошибка разблокировки ${user.chatId}:`, err);
+              return;
+            }
+            sendTelegramNotification(user.chatId, 'Ваш доступ восстановлен.');
+            console.log(`Разблокировка ${user.chatId}`);
           });
         }
       });
@@ -71,29 +100,21 @@ const chargeUsersHourly = () => {
 
 // Функция для отправки ежедневных уведомлений блокированным пользователям
 const sendDailyNotifications = () => {
-  db.query('SELECT * FROM users WHERE locked = 1 AND lastNotificationDate != ?', [moment().format('YYYY-MM-DD')], (err, users) => {
+  db.query('SELECT * FROM users WHERE locked = 1', (err, users) => {
     if (err) {
       console.error('Ошибка получения пользователей для уведомлений:', err);
       return;
     }
-
     users.forEach(user => {
-      //sendTelegramNotification(user.chatId, 'Ваш аккаунт заблокирован из-за недостаточного баланса. Пополните счет для восстановления доступа.');
+      sendTelegramNotification(user.chatId, 'Ваш аккаунт заблокирован из-за недостаточного баланса. Пополните счет для восстановления доступа.');
       console.log(`Уведомление ${user.chatId}`);
-      
-      db.query('UPDATE users SET lastNotificationDate = ? WHERE id = ?', [moment().format('YYYY-MM-DD'), user.id], (err) => {
-        if (err) {
-          console.error('Ошибка обновления даты последнего уведомления:', err);
-        }
-      });
     });
   });
 };
 
 // Задачи cron: списание каждый час и уведомления каждый день
-cron.schedule('0 * * * *', chargeUsersHourly); // Запуск каждый час в 00 минут
+cron.schedule('0 * * * *', chargeUsersHourly);
+cron.schedule('*/5 * * * *', UnlockUsers);
 cron.schedule(`0 ${TELEGRAM_NOTIFICATION_TIME.split(':')[1]} ${TELEGRAM_NOTIFICATION_TIME.split(':')[0]} * * *`, sendDailyNotifications);
 
-module.exports = { chargeUsersHourly, sendTelegramNotification };
-
-chargeUsersHourly();
+module.exports = { chargeUsersHourly, sendTelegramNotification, UnlockUsers };
